0e2a741f00af75bd3b93d693054c974ddf4b966b
Max Kofler <kofler.max.dev@gmail.com>
2022-11-06 08:24:59 -0800

    Made webresponse.js a little more verbose

--- src/scripts/webresponse.js
+++ src/scripts/webresponse.js
@@ -17,12 +17,14 @@ class WebResponse {
     }
 
     async fetch_data() {
+        console.debug("[WebRequest] Sending request: \"" + this.endpoint_url + "\"");
         const webresponse = await fetch(this.endpoint_url);
         const json_webresp = await webresponse.json();
 
         this.status = json_webresp["status"];
         this.response_code = json_webresp["response_code"];
         this.payload = json_webresp["payload"];  
+        console.debug("[WebRequest] Return status: " + this.status);
     }
 
 }
