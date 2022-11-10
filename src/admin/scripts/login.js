2d6f1c9fce83e09bd709bba720e2043406e86a5e
Max Kofler <kofler.max.dev@gmail.com>
2022-11-10 02:18:17 -0800

    Prepare login for new modal approach

--- src/admin/scripts/login.js
+++ src/admin/scripts/login.js
@@ -1,28 +1,24 @@
+async function f_login(){
+	userD = $("#username").val();
+	passD = $("#password").val();
 
-$(document).ready(function(){
+	await $.post(getBranchAPIURL() + "auth", {
+		user: userD,
+		pass: passD},
+		function(plain_res){
+			const res = jQuery.parseJSON(plain_res);
 
-	$("#loginform").on("submit", function(eventObj){
-		eventObj.preventDefault();
+			if(res.status != "SUCCESS") {
+				alert("Failure while attempting to log in: " + res.payload);
+				return;
+			}
 
-		userD = $("#username").val();
-		passD = $("#password").val();
-
-		$.post(getBranchAPIURL() + "auth", {
-			user: userD,
-			pass: passD},
-			function(plain_res){
-				const res = jQuery.parseJSON(plain_res);
-
-				if(res.status != "SUCCESS") {
-					alert("Failure while attempting to log in: " + res.payload);
-					return;
-				}
-
-				cur_authkey = res.payload;
-				setCookie("branch_authkey", cur_authkey, 1);
-				console.debug("New authkey: " + cur_authkey);
-				show_page("#btn_overview", "sites/overview.html");
-		});
+			cur_authkey = res.payload;
+			setCookie("branch_authkey", cur_authkey, 1);
+			console.debug("New authkey: " + cur_authkey);
 	});
-
-});
+	$("#modal_login_bd").modal("hide");
+	var evt = new CustomEvent("LoginStateChanged");
+	window.dispatchEvent(evt);
+	update_login();
+}
